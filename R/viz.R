b958a68d9dec8cf11a965b1b662194de82ea94d8
Riccardo Ceccaroni <riccardo.ceccaroni.95@gmail.com>
2026-06-15 10:12:42 +0200

    fix viz

--- R/viz.R
+++ R/viz.R
@@ -1,19 +1,34 @@
 
 vizdf = function(x, tz="EST") {
- cpu_active = function(x)
-   data.frame(tm = as.POSIXct(x$sampdate, tz=tz), xtype="CPU_MEM", 
-     pos="top", value = 100-x$`CPU_Idle%`, type="%CPU active")
- mem_used = function(x)
-   data.frame(tm = as.POSIXct(x$sampdate, tz=tz), xtype="CPU_MEM", 
-     pos="bot", value = x$MEM_Used, type="MEM used")
- net_KB_cum = function(x)
-   data.frame(tm = as.POSIXct(x$sampdate, tz=tz), xtype="NET_DSK",  
-     pos="top", value = (x$NET_RxKBTot+x$NET_TxKBTot), type="KB NET")
- dsk_KBwr_cum = function(x)
-   data.frame(tm = as.POSIXct(x$sampdate, tz=tz), xtype="NET_DSK", 
-     pos="bot", value = cumsum(x$DSK_WriteKBTot), type="Cumul KB disk")
- 
- rbind(cpu_active(x), mem_used(x), net_KB_cum(x), dsk_KBwr_cum(x))
+    cpu_active = function(x)
+    data.frame(tm = as.POSIXct(x$sampdate, tz=tz), xtype="CPU_MEM",
+        pos="top", value = 100-x$`CPU_Idle%`, type="%CPU active")
+    mem_used = function(x)
+    data.frame(tm = as.POSIXct(x$sampdate, tz=tz), xtype="CPU_MEM",
+        pos="bot", value = x$MEM_Used, type="MEM used")
+    net_KB_cum = function(x)
+    data.frame(tm = as.POSIXct(x$sampdate, tz=tz), xtype="NET_DSK",
+        pos="top", value = (x$NET_RxKBTot+x$NET_TxKBTot), type="KB NET")
+    dsk_KBwr_cum = function(x)
+    data.frame(tm = as.POSIXct(x$sampdate, tz=tz), xtype="NET_DSK",
+        pos="bot", value = cumsum(x$DSK_WriteKBTot), type="Cumul KB disk")
+
+   ans = rbind(cpu_active(x), mem_used(x), net_KB_cum(x), dsk_KBwr_cum(x))
+   if ("PROC_Pct" %in% names(x))
+     ans = rbind(ans, data.frame(tm=x$sampdate, xtype="PROCESS", pos="top",
+       value=x$PROC_Pct, type="%CPU process"))
+   if ("PROC_RSS" %in% names(x))
+     ans = rbind(ans, data.frame(tm=x$sampdate, xtype="PROCESS", pos="bot",
+       value=x$PROC_RSS, type="MEM process RSS"))
+   if ("PROC_RKB" %in% names(x))
+     ans = rbind(ans, data.frame(tm=x$sampdate, xtype="PROCESS", pos="top",
+       value=cumsum(replace(x$PROC_RKB, is.na(x$PROC_RKB), 0)),
+       type="Cumul KB process read"))
+   if ("PROC_WKB" %in% names(x))
+     ans = rbind(ans, data.frame(tm=x$sampdate, xtype="PROCESS", pos="bot",
+       value=cumsum(replace(x$PROC_WKB, is.na(x$PROC_WKB), 0)),
+       type="Cumul KB process write"))
+   ans
 }
 
 #' elementary display of usage data from collectl
@@ -25,6 +40,6 @@ vizdf = function(x, tz="EST") {
 #' plot_usage(lk)
 #' @export
 plot_usage = function(x) {
-  ggplot(vizdf(x), aes(x=tm, y=value)) + geom_point() + 
+  ggplot(vizdf(x), aes(x=tm, y=value)) + geom_point() +
           facet_grid(vars(type), scales="free")
 }
